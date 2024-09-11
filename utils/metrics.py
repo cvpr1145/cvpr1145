b676082bc0378bccbebb656f3150271a2f196d72
wacvsubmissions <161864782+wacvsubmissions@users.noreply.github.com>
2024-09-11 10:50:55 +0200

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -2,35 +2,9 @@ import torch
 from typing import List
 import numpy as np
 
-def uncertainty_estimation(preds:List, uc_type:str="absolute"):
-        preds = [x.detach() for x in preds]
-        y_stack = torch.stack(preds, dim=0)
-
-        if uc_type == "absolute":
-            # Get the number of tensors and the dimension
-            n, N, D = y_stack.shape
-            
-            # Expand tensors to compute pairwise differences
-            expanded_tensors_1 = y_stack.unsqueeze(0).expand(n, n, N, D)
-            expanded_tensors_2 = y_stack.unsqueeze(1).expand(n, n, N, D)
-
-            # Compute pairwise differences
-            pairwise_differences = expanded_tensors_1 - expanded_tensors_2
-
-            # Compute the norms along each dimension
-            pairwise_norms = torch.abs(pairwise_differences)
-            
-            # Exclude self-pairs (diagonal elements) by creating a mask
-            mask = torch.ones(n, n) - torch.eye(n)
-            
-            # Apply the mask to the pairwise norms
-            masked_pairwise_norms = pairwise_norms * mask.unsqueeze(-1).unsqueeze(-1)
-
-            # Compute the mean of the non-zero elements along each dimension
-            return masked_pairwise_norms.sum(dim=(0, 1)) / mask.sum()
-        
 
 def AULC(accs, uncertainties):
+    # copied from: https://github.com/cvlab-epfl/zigzag/blob/main/exps/notebooks/mnist_classification.ipynb 
     idxs = np.argsort(uncertainties)
     uncs_s = uncertainties[idxs]
     error_s = accs[idxs]
